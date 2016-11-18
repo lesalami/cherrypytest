93290bb2cfea8133ef4ca91fcb1f98ee38a015f2
lesalami <lesalami@gmail.com>
2016-11-18 16:05:03 +0000

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -1,11 +1,11 @@
 import cherrypy
-    import os
+import os
     
-    class HelloWorld(object):
-        def index(self):
-            return "Hello World!"
-        index.exposed = True
+class HelloWorld(object):
+    def index(self):
+        return "Hello World!"
+    index.exposed = True
 
-    cherrypy.config.update({'server.socket_host': '0.0.0.0',})
-    cherrypy.config.update({'server.socket_port': int(os.environ.get('PORT', '5000')),})
-    cherrypy.quickstart(HelloWorld())
+cherrypy.config.update({'server.socket_host': '0.0.0.0',})
+cherrypy.config.update({'server.socket_port': int(os.environ.get('PORT', '5000')),})
+cherrypy.quickstart(HelloWorld())
